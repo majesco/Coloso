854e8f6eab25db1d3b4eb7d0964fee4ec8b98514
majesco <nicolas.j2007@gmail.com>
2016-09-25 20:33:49 -0600

    Graphical User Interface commit 1 of Coloso

---
+++ Coloso/src/coloso/Coloso.java
@@ -0,0 +1,18 @@
+
+package coloso;
+
+/**
+ *
+ * @author nicolasjimenez
+ */
+public class Coloso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("bla bla");
+    }
+    
+}
